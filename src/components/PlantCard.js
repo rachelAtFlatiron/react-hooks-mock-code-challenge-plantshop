b81f35ea375878d2d3a2c6f02dc0df52f476a254
Rachel Katz <rachel.katz@flatironschool.com>
2023-05-09 09:30:01 -0400

    added delete and price input for advanced deliverables

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -6,11 +6,13 @@ function PlantCard() {
       <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
       <h4>{"plant name"}</h4>
       <p>Price: {"plant price"}</p>
+      <input type="number" step="0.01" name="price" placeholder="new price" />
       {true ? (
         <button className="primary">In Stock</button>
       ) : (
         <button>Out of Stock</button>
       )}
+      <button style={{background: "red", color: "white"}}>Delete Plant</button>
     </li>
   );
 }
